b376f0a8cc88f92413380324c0cc9afe77289811
Alan <alin1@2u.com>
2014-05-29 14:06:35 -0400

    added bash configs

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Syncing dotfiles..."
-for f in .vimrc .vim .gitconfig
+for f in .vimrc .vim .gitconfig .aliases .bash_prompt .bash_profile .bashrc
 do
     ln -fnivs $PWD/$f $HOME/$f
 done
