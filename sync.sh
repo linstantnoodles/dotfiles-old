d3eb64a8180a7eb553cf4673d80ee042ca2a8dc0
Alan <alin1@2u.com>
2014-06-02 14:42:06 +0000

    added tmux conf

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Syncing dotfiles..."
-for f in .vimrc .vim .gitconfig .aliases .bash_prompt .bash_profile .bashrc
+for f in .vimrc .vim .gitconfig .aliases .bash_prompt .bash_profile .bashrc .tmux.conf
 do
     ln -fnivs $PWD/$f $HOME/$f
 done
